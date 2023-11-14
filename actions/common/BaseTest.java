029ec94480f5d6c43354f789af7e1059a2eacf69
HaiDang <phamhaidang010896@gmail.com>
2023-11-14 16:36:21 +0700

    init project

---
+++ actions/common/BaseTest.java
@@ -0,0 +1,92 @@
+package common;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openqa.selenium.WebDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseTest {
+	private WebDriver driver;
+	protected final Log log;
+
+	protected WebDriver getBroserDriver(String browserName) {
+		if (browserName.contains("chrome")) {
+			driver = WebDriverManager.chromedriver().create();
+		}
+
+		else if (browserName.contains("firefox")) {
+			driver = WebDriverManager.firefoxdriver().create();
+		}
+
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://demo.nopcommerce.com/");
+		return driver;
+	}
+
+	protected BaseTest() {
+		log = LogFactory.getLog(getClass());
+	}
+
+	protected void closeBrowserDriver() {
+		String cmd = null;
+		try {
+			String osName = System.getProperty("os.name").toLowerCase();
+			log.info("OS name = " + osName);
+
+			String driverInstanceName = driver.toString().toLowerCase();
+			log.info("Driver instance name = " + driverInstanceName);
+
+			String browserDriverName = null;
+
+			if (driverInstanceName.contains("chrome")) {
+				browserDriverName = "chromedriver";
+			} else if (driverInstanceName.contains("internetexplorer")) {
+				browserDriverName = "IEDriverServer";
+			} else if (driverInstanceName.contains("firefox")) {
+				browserDriverName = "geckodriver";
+			} else if (driverInstanceName.contains("edge")) {
+				browserDriverName = "msedgedriver";
+			} else if (driverInstanceName.contains("opera")) {
+				browserDriverName = "operadriver";
+			} else {
+				browserDriverName = "safaridriver";
+			}
+
+			if (osName.contains("window")) {
+				cmd = "taskkill /F /FI \"IMAGENAME eq " + browserDriverName + "*\"";
+			} else {
+				cmd = "pkill " + browserDriverName;
+			}
+
+			if (driver != null) {
+				driver.manage().deleteAllCookies();
+				driver.quit();
+			}
+		} catch (Exception e) {
+			log.info(e.getMessage());
+		} finally {
+			try {
+				Process process = Runtime.getRuntime().exec(cmd);
+				process.waitFor();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	protected void sleepInSecond(long timeInSecond) {
+		try {
+			Thread.sleep(timeInSecond * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ testcases/nopcommerce/user/Register.java
@@ -0,0 +1,28 @@
+package nopcommerce.user;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import common.BaseTest;
+
+public class Register extends BaseTest {
+	WebDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {
+		getBroserDriver("chrome");
+		sleepInSecond(3);
+	}
+
+	@Test
+	public void TC_01() {
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+		closeBrowserDriver();
+	}
+}
